d7c7885a4890a84ce28949a0646eebf3b5ec59b4
nautilus@narwhal <nautilus@trovaprezzi.it>
2022-05-26 09:55:11 +0200

    organization aggregateRating

--- lib/schema_dot_org/organization.rb
+++ lib/schema_dot_org/organization.rb
@@ -6,11 +6,15 @@ require 'schema_dot_org'
 #
 module SchemaDotOrg
   class Organization < Thing
-    attr_accessor :logo
+    attr_accessor :logo,
+                  :aggregate_rating
+
+    validates :aggregate_rating, type: SchemaDotOrg::AggregateRating, allow_nil: true
 
     def _to_json_struct
       output_logo = self.logo.is_a?(String) ? self.logo : self.logo&.to_json_struct
       super.merge({
+                    "aggregateRating" => aggregate_rating&.to_json_struct,
                     "logo" => output_logo
                   })
     end
