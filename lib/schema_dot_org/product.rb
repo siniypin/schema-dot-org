76ee3f93f5e4060154f9a1faca1a3211c066a9db
Boris Tveritnev <btveritnev@cflox.com>
2022-07-05 20:03:20 +0200

    added few new elements

--- lib/schema_dot_org/product.rb
+++ lib/schema_dot_org/product.rb
@@ -11,9 +11,31 @@ module SchemaDotOrg
                   :offers,
                   :brand,
                   :review,
-                  :additional_property
+                  :additional_property,
+                  :category,
+                  :color,
+                  :material,
+                  :productID,
+                  :gtin,
+                  :sku,
+                  :height,
+                  :width,
+                  :depth,
+                  :size,
+                  :keywords
 
-    validates :offers,                      type: SchemaDotOrg::AggregateOffer, allow_nil: true
+    validates :category,                    allow_nil: true
+    validates :color,                       allow_nil: true
+    validates :material,                    allow_nil: true
+    validates :productID,                   allow_nil: false
+    validates :gtin,                        allow_nil: true
+    validates :sku,                         allow_nil: true
+    validates :height,                      type: SchemaDotOrg::QuantitativeValue, allow_nil: true
+    validates :width,                       type: SchemaDotOrg::QuantitativeValue, allow_nil: true
+    validates :depth,                       type: SchemaDotOrg::QuantitativeValue, allow_nil: true
+    validates :size,                        type: SchemaDotOrg::SizeSpecification, allow_nil: true
+    validates :keywords,                    allow_nil: true
+    validates :offers,                      type: SchemaDotOrg::Offer, allow_nil: true
     validates :aggregate_rating,            type: SchemaDotOrg::AggregateRating, allow_nil: true
     validates :brand,                       type: SchemaDotOrg::Thing, allow_nil: true
     validates :review,                      type: Array, allow_nil: true
@@ -21,9 +43,19 @@ module SchemaDotOrg
 
     def _to_json_struct
       super.merge({
+          "productID" => productID,
+          "gtin" => gtin,
+          "sku" => sku,
+          "category" => category,
+          "brand" => brand&.to_json_struct,
+          "color" => color,
+          "material" => material,
+          "height" => height&.to_json_struct,
+          "width" => width&.to_json_struct,
+          "depth" => depth&.to_json_struct,
+          "size" => size&.to_json_struct,
           "aggregateRating" => aggregate_rating&.to_json_struct,
           "offers" => offers&.to_json_struct,
-          "brand" => brand&.to_json_struct,
           "review" => review&.map(&:to_json_struct),
           "additionalProperty" => additional_property&.map(&:to_json_struct)
       })
---
+++ lib/schema_dot_org/quantitative_value.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'schema_dot_org'
+
+module SchemaDotOrg
+  class QuantitativeValue < Thing
+    #TODO: add props
+  end
+end
